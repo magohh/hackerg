cce71dc2bc756dead52ddf4ff97832d5f7a5ee83
magohh <mago_hdz_h@outlook.es>
2022-03-23 21:30:28 -0600

    TiendaDeRegalos/Hernandez

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+
+import logo from '../img/logo.png';
+
+const NavBar = () =>{
+	return(
+	<div>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#"><img className="logo" src={logo}></img></a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+					<ul className="navbar-nav">
+					<li className="nav-item">
+						<a className="nav-link active" aria-current="page" href="#">Como funciona</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Diseña tu tarjeta</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Tarjetas</a>
+					</li>
+					</ul>
+					<a className="nav-link">Iniciar sesion</a>
+				</div>
+			</div>
+		</nav>
+	</div>
+	)
+}
+
+export default NavBar
