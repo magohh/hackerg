7a35ba43726f084587c72856703c939a1f5283fa
magohh <mago_hdz_h@outlook.es>
2022-03-30 21:06:09 -0600

    previo a icono bien

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar.js'
+import NavBar from './components/NavBar.js';
+// import IconButton from '@mui/material/IconButton';
+// import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/icons-material/Icon';
+
 
 
 
 function App() {
   return (
+    <>
     <NavBar/>
+    <IconButton color="primary" aria-label="add to shopping cart">
+  <AddShoppingCartIcon />
+</IconButton>
+<DeleteIcon/>
+</>
   );
 }
 
