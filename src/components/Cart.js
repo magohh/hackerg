b51dccec9f13372109aaa3a77d60121de5a94d19
magohh <mago_hdz_h@outlook.es>
2022-04-21 13:17:40 -0500

    Hackergift/Hernandez

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,11 +5,12 @@ import {Link} from 'react-router-dom'
 
 const Cart = ()=>{
 	const info = useContext(CartContext)
+	
 	return(
 		<div className='container'>
 			<div className='row'>
-				<div className='col-md-12 text-center mt-3'>
-					<h4>Carrito</h4>
+				<div className='col-md-12 text-center mt-5'>
+					<h3>Carrito</h3>
 				</div>
 				<div className='col-md-12'>
 					
@@ -19,7 +20,7 @@ const Cart = ()=>{
 						? 
 						<div className='d-flex justify-content-between'>
 							<Link to="/">Seguir comprando</Link>
-							<button onClick={info.removeCart}>Borrar todo</button>
+							<button className="btn btn-primary mt-4" onClick={info.removeCart}>Borrar todo</button>
 							
 						</div>
 						: <p className='text-center'>Tu carrito está vacío</p>
@@ -33,16 +34,16 @@ const Cart = ()=>{
 				<div className="container mt-5">
 					{
 						info.cartList.map(item =>
-							<div className="row">
+							<div className="row mt-5">
 								
 								<div className="col-md-3">
 									<img className="cart_img" src={item.imgItem}></img>
 								</div>
-								<div className="col-md-9 text-center">
-								<p className="item_detail_title mt-3">{item.nameItem}</p>
-								<p className="item_detail_price">${item.costItem}</p>
+								<div className="col-md-3 text-left">
+								<p className="item_detail_title">{item.nameItem}</p>
+								<p className="item_detail_price">${item.costItem} c/u</p>
 								<p>{item.qtyItem} seleccionados</p>
-								<button onClick={() => info.deleteI(item.idItem)}>Borrar Item</button>
+								<button className="btn btn-primary mt-2" onClick={() => info.deleteI(item.idItem)}>Borrar Item</button>
 								
 								</div>
 							</div>
