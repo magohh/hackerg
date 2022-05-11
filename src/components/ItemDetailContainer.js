0d2bb982fd80b1fa25da5e780134cbf7ad3a4c88
magohh <mago_hdz_h@outlook.es>
2022-05-10 21:12:33 -0500

    Hackergift/Hernandez

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,8 +1,7 @@
 import {useEffect,useState} from 'react'
 import { useParams } from "react-router";
 import ItemDetail from './ItemDetail';
-// import getItem from '../utils/promise'
-// const {products} = require('../utils/products');
+
 
 import { doc, getDoc } from "firebase/firestore";
 import db from '../utils/firebaseConfig'
@@ -22,7 +21,6 @@ const ItemDetailContainer = ()=>{
                     ...docSnap.data()
                 }
             } else {
-            // doc.data() will be undefined in this case
             console.log("No such document!");
             }
         }
@@ -32,16 +30,6 @@ const ItemDetailContainer = ()=>{
         
     },[]) 
 
-
-    // useEffect (()=>{
-        
-    //     getItem(2000,products.find(item => item.id === parseInt(idItem)))
-    //         .then(result => setDato(result))
-    //         .catch(error => console.log(error))
-        
-    // },[])
-    // .filter(item => item.id === parseInt(idItem)), console.log(idItem)
-    // .find(item => item.id === parseInt(idItem))
     return(
         <>
             <ItemDetail item={dato}/>  
